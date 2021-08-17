aac61013b7019942b1d7012464ded9f5bbbed0b2
Sibints2000 <sibints2000@gmail.com>
2021-08-17 19:07:34 +0530

    Twenty Second Commit

--- main.py
+++ main.py
@@ -24,7 +24,11 @@ while is_race_on:
 
     for turtle in all_turtles:
         if turtle.xcor() > 230:
-            print(turtle.color())
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've Won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've Lost. The {winning_color} turtle is the winner.")
 
 
         rand_distance = random.randint(0, 10)
