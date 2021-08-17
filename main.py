12dcfadd37d0e1f8d59ff6ba11d9a4db0b19f522
Sibints2000 <sibints2000@gmail.com>
2021-08-17 17:32:44 +0530

    Twelfth Commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter the color: ")
-
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 tim = Turtle(shape="turtle")
 tim.penup()
