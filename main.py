26fc0aa94bc11afc647184aa75d5c4ea138874a6
Sibints2000 <sibints2000@gmail.com>
2021-08-17 17:24:41 +0530

    Fifth Commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
-
+screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter the color: ")
 
 
 screen.exitonclick()
